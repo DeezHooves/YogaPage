ff7abd50c2278f04b0c7d7c034d68cb80f8ad001
Deezhooves <alexg93x2@gmail.com>
2022-07-16 21:28:33 -0500

    Added edit functionality to steps

--- routes/steps.js
+++ routes/steps.js
@@ -44,6 +44,28 @@ router.post("/", isLoggedIn, (req, res) => {
     });
 });
 
+// edit a step
+router.get("/:step_id/edit", (req, res) => {
+    step.findById(req.params.step_id, (err, foundStep) => {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.render("steps/edit", {routine_id: req.params.id, step: foundStep});
+        }
+    });
+});
+
+// update a step
+router.put("/:step_id", (req, res) => {
+    step.findByIdAndUpdate(req.params.step_id, req.body.step, (err, updatedStep) => {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/routines/" + req.params.id);
+        }
+    });
+});
+
 // delete a step
 router.get("/delete", isLoggedIn, (req, res) => {
     console.log("Delete stuff");
