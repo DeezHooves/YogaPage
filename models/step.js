39bc6dc570d03dbdded6df2dc9db889b375c7ac6
Deezhooves <alexg93x2@gmail.com>
2022-07-21 13:52:15 -0500

    Styled index page

--- models/step.js
+++ models/step.js
@@ -3,7 +3,8 @@ let Schema = mongoose.Schema;
 
 // SCHEMA SETUP
 let stepSchema = new mongoose.Schema({
-    image: String,
+    image1: String,
+    image2: String,
     text: String,
     poster: {
         id:{
